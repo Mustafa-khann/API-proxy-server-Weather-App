39a7f940df5bbdd16aef678f668462560a6068e5
Mustafa <mkafridi073@gmail.com>
2021-10-24 13:33:11 +0500

    .

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const needle = require('needle')
 
 // Env variable 
-
+const API_BASE_URL =process.env.API_BASE_URL
+const API_KEY_NAME =process.env.API_KEY_NAME
+const API_KEY_VALUE =process.env.API_KEY_VALUE
 
 
 router.get('/', async (req, res) => {
