e48f3618daf73bbadd8d8f15cdd66bd5b476a8c2
Mustafa <mkafridi073@gmail.com>
2021-10-24 17:10:56 +0500

    completed

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ const url = require('url')
 const express = require('express');
 const router = express.Router();
 const needle = require('needle')
+const apicache = require('apicache')
 
 // Env variable 
 const API_BASE_URL =process.env.API_BASE_URL
 const API_KEY_NAME =process.env.API_KEY_NAME
 const API_KEY_VALUE =process.env.API_KEY_VALUE
 
+let cache = apicache.middleware
 
-router.get('/', async (req, res) => {
+
+router.get('/', cache('2 minutes') ,async (req, res) => {
     try
     {
         const params = new URLSearchParams({
@@ -20,6 +23,11 @@ router.get('/', async (req, res) => {
         const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
         const data = apiRes.body
     
+        // Log the request to public API
+        if(process.env.NODE_ENV !== 'production')
+        {
+            console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+        }
         res.status(200).json(data)
     }
     catch (error)
